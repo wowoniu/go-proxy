648148ca180308ad97187a04123d1ae8c41f0139
wowoniu <zhiqiangvip999@gmail.com>
2019-04-03 19:02:59 +0800

    fix

--- client/client.go
+++ client/client.go
@@ -2,46 +2,57 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net"
+	"regexp"
+	"time"
 )
 
 type Tunnel struct {
-	AppID      string
 	ClientConn net.Conn
-	LocalConn  net.Conn
 }
 
 var (
-	proxyTunnel *Tunnel
-	serverAddr  string
-	proxyPort   string
+	proxyTunnel  *Tunnel
+	serverAddr   string
+	proxyPort    string
+	proxySession map[string]net.Conn
+	buffSize     int = 102400
 )
 
 func main() {
-	flag.StringVar(&serverAddr, "serverAddr", "127.0.0.1:9000", "代理服务器地址")
+	flag.StringVar(&serverAddr, "serverAddr", "localhost:9000", "代理服务器地址")
 	flag.StringVar(&proxyPort, "proxyPort", "80", "代理端口")
-	go connectToServer()
-	for {
-	}
+	proxySession = make(map[string]net.Conn)
+	defer func() {
+		for _, conn := range proxySession {
+			conn.Close()
+		}
+	}()
+	connectToServer()
 }
 
 func connectToServer() {
-	tcpAddr, _ := net.ResolveTCPAddr("tcp4", serverAddr)
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	//tcpAddr, _ := net.ResolveTCPAddr("tcp4", serverAddr)
+	log.Println("connect to ", serverAddr, "...")
+	conn, err := net.Dial("tcp", serverAddr)
 	if err != nil {
 		log.Fatal("connected error :", err)
 	}
 	defer conn.Close()
 	log.Println("server connect success")
+	//与服务器建立连接通道
 	proxyTunnel = &Tunnel{
 		ClientConn: conn,
 	}
 
-	buff := make([]byte, 512)
+	//读取服务器代理请求消息
 	for {
+		buff := make([]byte, 1024)
 		if n, err := conn.Read(buff); err != nil {
 			log.Fatal("READ FROM SERVER ERROR:", err)
+			return
 		} else {
 			go handleRequest(buff[:n])
 		}
@@ -50,33 +61,57 @@ func connectToServer() {
 }
 
 func handleRequest(data []byte) {
-	if proxyTunnel.LocalConn == nil {
-		//建立本地连接
-		tcpAddr, _ := net.ResolveTCPAddr("tcp4", "127.0.0.1:"+proxyPort)
-		conn, err := net.DialTCP("tcp", nil, tcpAddr)
-		if err != nil {
-			log.Println("connected error :", err)
+	requestID := data[:16]
+	requestData := data[16:]
+	log.Printf("接收到代理请求:%x", requestID)
+	// log.Printf("接收到代理请求:%s", requestData)
+	conn, err := net.Dial("tcp", "localhost:"+proxyPort)
+	if err != nil {
+		log.Println("connected error :", err)
+		return
+	}
+	defer conn.Close()
+	log.Println("local connect success:", fmt.Sprintf("%x", requestID))
+	proxySession[string(requestID)] = conn
+	//读取本地连接的响应
+	str := string(requestData)
+	//host替换
+	reg := regexp.MustCompile(`Host: ([\s\S]*?)[:\d+]?\r\n`)
+	str = reg.ReplaceAllString(str, "Host: local.jjs.ie:80\r\n")
+	// 本地连接 写入请求
+	conn.Write([]byte(str))
+	//读取本地连接的响应
+
+	for {
+		buff := make([]byte, buffSize)
+		conn.SetReadDeadline(time.Now().Add(time.Second * 5))
+		if n, err := conn.Read(buff); err != nil {
+			//本地连接已关闭
+			log.Println("READ FROM LOCAL ERROR:", err)
+			go handleLocalResponse(requestID, []byte(""))
+			return
+		} else {
+			log.Println("请求长度:", n)
+			response := make([]byte, n+16)
+			response = append(requestID, buff[:n]...)
+			go handleLocalResponse(requestID, response)
 			return
 		}
-		defer conn.Close()
-		log.Println("server connect success")
-		proxyTunnel.LocalConn = conn
-
-		buff := make([]byte, 512)
-		go func() {
-			for {
-				if n, err := conn.Read(buff); err != nil {
-					log.Println("READ FROM LOCAL ERROR:", err)
-					return
-				} else {
-					go handleLocalResponse(buff[:n])
-				}
-			}
-		}()
 	}
-	proxyTunnel.LocalConn.Write(data)
+
 }
 
-func handleLocalResponse(data []byte) {
+func handleLocalResponse(requestID []byte, data []byte) {
+	log.Printf("本地响应：%x", data[:16])
+	proxyTunnel.ClientConn.Write(data)
+	//关闭本地连接
+	closeLocalConn(requestID)
+}
 
+func closeLocalConn(requestID []byte) {
+	if conn, ok := proxySession[string(requestID)]; ok {
+		conn.Close()
+		delete(proxySession, string(requestID))
+		log.Printf("关闭本地连接：%x", requestID)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+	"crypto/md5"
 	"flag"
+	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"time"
 )
 
 type Tunnel struct {
-	AppID       string
-	RequestConn net.Conn
-	ClientConn  net.Conn
+	ClientConn net.Conn
 }
 
 var (
-	proxyTunnel *Tunnel
-	clientPort  string
-	proxyPort   string
+	proxyTunnel  *Tunnel
+	clientPort   string
+	proxyPort    string
+	proxySession map[string]net.Conn
+	buffSize     int = 102400
 )
 
 func main() {
-	flag.StringVar(&clientPort, "clientPort", "9000", "客户端连接端口")
-	flag.StringVar(&proxyPort, "proxyPort", "80", "代理端口")
+	flag.StringVar(&clientPort, "clientPort", "9000", "客户端连接的端口")
+	flag.StringVar(&proxyPort, "proxyPort", "8082", "代理端口")
+	proxyTunnel = &Tunnel{}
+	proxySession = make(map[string]net.Conn)
+
+	defer func() {
+		for _, conn := range proxySession {
+			conn.Close()
+		}
+		closeClientConn()
+	}()
 	go listenClient()
 	go listenProxyRequest()
 	for {
@@ -29,39 +42,46 @@ func main() {
 
 func listenClient() {
 	//客户端连接监听
+	log.Println("监听客户端端口:", clientPort)
 	clientListener, err := net.Listen("tcp", ":"+clientPort)
 	if err != nil {
 		log.Fatal("client connect port listen error:", err)
 	}
 	defer clientListener.Close()
-	buf := make([]byte, 10240)
 	for {
-		conn, err := clientListener.Accept()
-		if err != nil {
+		log.Println("等待客户端连接")
+		if conn, err := clientListener.Accept(); err != nil {
 			//连接建立失败
 			log.Println("client connect error:", err)
 			continue
-		}
-		//客户端连接建立成功则创建成功一个TUNNEL
-		proxyTunnel = &Tunnel{
-			AppID:      "",
-			ClientConn: conn,
-		}
-		//开启协程 读取客户端的响应数据
-		go func() {
-			buff := make([]byte, 10240)
-			for {
-				n, err := conn.Read(buff)
-				if err != nil {
-					//读取失败
-					log.Println("client response read error:", err)
-					proxyTunnel.ClientConn = nil
-					return
+		} else {
+			//客户端连接建立成功则创建成功一个TUNNEL
+			fmt.Println("客户端连接成功")
+			proxyTunnel.ClientConn = conn
+			//开启协程 处理客户端数据通信
+			clientClosed := make(chan bool)
+			go func(clientClosed chan bool) {
+				for {
+					buff := make([]byte, buffSize)
+					n, err := conn.Read(buff)
+					if err != nil {
+						//读取失败 端口与客户端的连接
+						log.Println("client response read error:", err)
+						if proxyTunnel.ClientConn != nil {
+							proxyTunnel.ClientConn.Close()
+							proxyTunnel.ClientConn = nil
+						}
+						clientClosed <- true
+						return
+					}
+					//对响应进行转发处理
+					// fmt.Println(string(buff[:n]))
+					go handleClientResponse(buff[:n])
 				}
-				//对响应进行转发处理
-				go handleClientResponse(buf[:n])
-			}
-		}()
+			}(clientClosed)
+			<-clientClosed
+		}
+
 	}
 
 }
@@ -73,38 +93,77 @@ func listenProxyRequest() {
 		log.Fatal("request proxy port listen error:", err)
 	}
 	defer requestListener.Close()
+	log.Println("监听代理端口:", proxyPort)
 	//等待代理请求建立连接
 	for {
-		requestConn, err := requestListener.Accept()
-		if err != nil {
+		log.Println("等待代理请求")
+		if requestConn, err := requestListener.Accept(); err != nil {
 			log.Println("proxy request connect error:", err)
 			continue
-		}
-		//开启协程 读取代理请求的数据
-		go func() {
-			buff := make([]byte, 10240)
-			for {
-				n, err := requestConn.Read(buff)
-				if err != nil {
-					//连接可能关闭
-					log.Println("proxy request read error:", err)
-					proxyTunnel.RequestConn = nil
-					return
+		} else {
+			//开启协程 读取代理请求的数据
+			go func(conn net.Conn) {
+				requestID := md5.Sum([]byte(fmt.Sprintf("%v%v", time.Now().UnixNano(), rand.Intn(1000))))
+				proxySession[string(requestID[:])] = conn
+				log.Println("代理请求连接建立:", fmt.Sprintf("%x", requestID))
+				for {
+					buff := make([]byte, 1024)
+					n, err := conn.Read(buff)
+					if err != nil {
+						//请求连接关闭
+						log.Println("proxy request read error:", err)
+						closeRequestConn(requestID[:])
+						return
+					}
+					//处理代理请求的数据
+					data := make([]byte, n+16)
+					data = append(requestID[:], buff[:n]...)
+					log.Printf("接收到请求:%x", requestID[:])
+					go handleProxyRequest(data)
+
 				}
-				//处理代理请求的数据
-				go handleProxyRequest(buff[:n])
-
-			}
-		}()
-
+			}(requestConn)
+		}
 	}
 
 }
 
 func handleClientResponse(data []byte) {
-	proxyTunnel.RequestConn.Write(data)
+	//前16个字节长度为requestID
+	requestID := data[:16]
+	responseData := data[16:]
+	fmt.Printf("客户端响应:%x\n", data[:16])
+	if conn, ok := proxySession[string(requestID)]; ok {
+		conn.Write(responseData)
+		conn.Close()
+		delete(proxySession, string(requestID))
+	} else {
+		log.Printf("请求连接已断开 响应失败:%x", requestID)
+	}
 }
 
 func handleProxyRequest(data []byte) {
-	proxyTunnel.ClientConn.Write(data)
+	if proxyTunnel.ClientConn != nil {
+		log.Println("代理请求 写入客户端连接")
+		// log.Println("代理请求内容:")
+		// log.Println(string(data))
+		proxyTunnel.ClientConn.Write(data)
+	}
+
+}
+
+func closeRequestConn(requestID []byte) {
+	log.Printf("请求连接关闭:%x", requestID)
+	if conn, ok := proxySession[string(requestID)]; ok {
+		conn.Close()
+		delete(proxySession, string(requestID))
+	}
+}
+
+func closeClientConn() {
+	log.Println("关闭客户端连接")
+	if proxyTunnel.ClientConn != nil {
+		proxyTunnel.ClientConn.Close()
+		proxyTunnel.ClientConn = nil
+	}
 }
